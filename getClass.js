1ed8d2147708f03b28b6394431c2562db872186e
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-02-25 11:44:59 -0700

    first commit

---
+++ getClass.js
@@ -0,0 +1,34 @@
+console.log("start");
+
+console.log("get up from bed");
+
+let bathLastNight = true;
+
+console.log("Did you bath last night", bathLastNight);
+
+if(bathLastNight){
+    console.log("dress up for class");
+    console.log("Eat breakfast");
+} else{
+    console.log("Bath and brush your teeth");
+}
+
+let weatherIsGood = true;
+
+console.log("Is the weather good", weatherIsGood);
+
+if(weatherIsGood){
+    console.log("get in the car and drive to class");
+
+} else {
+    console.log("clean the car");
+}
+
+console.log("Park your car");
+
+console.log("Get out of the car");
+
+console.log("Enter the class");
+
+console.log("End process");
+
